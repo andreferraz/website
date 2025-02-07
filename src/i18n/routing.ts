3bc16a0355eb7945f1c68cb402af8f2a2409b961
André Ferraz <hello@andreferraz.dev>
2025-02-06 22:41:40 -0300

    Remove default locale prefix

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,9 +1,24 @@
 import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
+type Locale = 'en' | 'pt_BR';
+
+type LocaleInfo = {
+  locale: Locale;
+  label: string;
+  acronym?: string;
+  default: boolean;
+};
+
+export const locales: LocaleInfo[] = [
+  { locale: 'en', label: 'English', acronym: 'EN', default: true },
+  { locale: 'pt_BR', label: 'Português (Brasil)', acronym: 'PT', default: false },
+];
+
 export const routing = defineRouting({
-  locales: ['en', 'pt_BR'],
-  defaultLocale: 'en',
+  locales: locales.map((i) => i.locale),
+  defaultLocale: locales.find((i) => i.default)!.locale,
+  localePrefix: 'as-needed',
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
