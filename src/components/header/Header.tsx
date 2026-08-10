efa1e4b47191ba47f3a366bc3558816f744e5cf1
André Ferraz <15928408+andreferraz@users.noreply.github.com>
2026-08-10 20:28:30 -0300

    Make CEV name section sticky and review z-index

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,21 +12,25 @@ interface HeaderProps {
 
 export const Header = ({ className = '', alternates }: HeaderProps) => {
   return (
-    <header
-      className={`${className} w-full fixed top-0 z-10 bg-(--background) shadow-[0px_60px_60px_var(--header-shadow),0px_30px_30px_var(--header-shadow)]`}
-    >
-      <div className="container flex items-center relative h-(--header-height)">
-        <ScreenReaderLink className="fixed! top-0 left-[50%] translate-x-[-50%]" />
-        <div className="flex justify-between items-center w-full pt-3">
-          <Link href="/" className="inline-flex items-center text-(--foreground)">
-            <Logo />
-          </Link>
-          <div className="flex items-center gap-4">
-            <LanguageSwitch alternates={alternates} />
-            <ThemeToggle />
+    <>
+      <div
+        aria-hidden="true"
+        className="pointer-events-none fixed top-0 left-0 right-0 h-(--header-height) z-20 shadow-[0px_60px_60px_var(--header-shadow),0px_30px_30px_var(--header-shadow)]"
+      />
+      <header className={`${className} w-full fixed top-0 z-30 bg-(--background)`}>
+        <div className="container flex items-center relative h-(--header-height)">
+          <ScreenReaderLink className="fixed! top-0 left-[50%] translate-x-[-50%]" />
+          <div className="flex justify-between items-center w-full pt-3">
+            <Link href="/" className="inline-flex items-center text-(--foreground)">
+              <Logo />
+            </Link>
+            <div className="flex items-center gap-4">
+              <LanguageSwitch alternates={alternates} />
+              <ThemeToggle />
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   )
 }
