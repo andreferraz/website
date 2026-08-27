a463ab4ae48efe74d70574d93afcf29d9bccabf5
André Ferraz <15928408+andreferraz@users.noreply.github.com>
2026-08-26 21:16:36 -0300

    Write pt-BR version of the CEV article

---
+++ src/content/articles/ui-naming-convention/uma-convencao-de-nomenclatura-para-componentes-de-ui.pt-BR.meta.ts
@@ -0,0 +1,10 @@
+import type { ArticleMeta } from '@/utils/typings/ArticleMeta'
+import { Tag } from '@/utils/typings/Tag'
+
+export const meta: ArticleMeta = {
+  title: 'Uma convenção de nomenclatura para componentes de UI',
+  date: '2021-04-30',
+  tags: [Tag.FRONTEND, Tag.ARCHITECTURE, Tag.COMPONENTS],
+  excerpt:
+    'Dar nomes às coisas na programação é difícil. CEV é uma convenção de nomenclatura que traz clareza e consistência para sua biblioteca de componentes de UI.',
+}
